d16c7f3981a96faaee2edb66edd0d6da5c5eee2c
shinwc147369@gmail.com <shinwc147369@gmail.com>
2024-01-11 10:37:22 +0900

    작업 16, 나머지 기능들 구조 개선

--- src/com/KoreaIT/java/JDBCAM/service/ArticleService.java
+++ src/com/KoreaIT/java/JDBCAM/service/ArticleService.java
@@ -1,19 +1,35 @@
 package com.KoreaIT.java.JDBCAM.service;
 
 import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
 
+import com.KoreaIT.java.JDBCAM.Article;
 import com.KoreaIT.java.JDBCAM.dao.ArticleDao;
 
 public class ArticleService {
-
 	private ArticleDao articleDao;
-
 	public ArticleService(Connection conn) {
 		this.articleDao = new ArticleDao(conn);
 	}
-
 	public int doWrite(String title, String body) {
 		return articleDao.doWrite(title, body);
+	}
+
+	public Map<String, Object> getArticleById(int id) {
+		return articleDao.getArticleById(id);
+	}
+
+	public void doDelete(int id) {
+		articleDao.doDelete(id);
+	}
+
+	public void doUpdate(int id, String title, String body) {
+		articleDao.doUpdate(id, title, body);
+	}
+
+	public List<Article> getArticles() {
+		return articleDao.getArticles();
 	}
 
 }
